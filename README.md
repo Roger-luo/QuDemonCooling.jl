656a255e6fdb44a7ee86955cd6a3dcce564035a6
Roger-luo <hiroger@qq.com>
2016-10-06 18:41:21 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # QuDemonCooling
+
+This Package is for simulating Demon-like algorithm quantum cooling.
+
+The package is not included in METADATA.jl. Therefore, you need to use `Pkg.clone` to download this package from Github.
+
+use this:
+
+```
+Pkg.clone("git@github.com:Roger-luo/QuDemonCooling.jl.git")
+```
+
+Good luck!
--- src/QuDemonCooling.jl
+++ src/QuDemonCooling.jl
@@ -1,5 +1,30 @@
 module QuDemonCooling
+using QuBase
+import QuBase.AbstractQuVector
+export cooling!
 
-# package code goes here
+# for general use
+function cooling!(state::AbstractQuVector,U::AbstractMatrix,t::Real,gamma::Real)
+    dice = rand()
+    if dice <= 0.5*(1+sin(gamma))
+        heat!(state,U,t,gamma)
+        return 0.5*(1+sin(gamma))
+    else
+        cool!(state,U,t,gamma)
+        return 0.5*(1-sin(gamma))
+    end
+end
+
+function heat!(state::AbstractQuVector,U::AbstractMatrix,t::Real,gamma::Real)
+    next_state = 0.5*( coeffs(state)+im*exp(im*gamma)*U*coeffs(state) ) |> normalize!
+    CQST = QuBase.similar_type(state)
+    return CQST(next_state,bases(state))
+end
+
+function cool!(state::AbstractQuVector,U::AbstractMatrix,t::Real,gamma::Real)
+    next_state = 0.5*( coeffs(state)-im*exp(im*gamma)*U*coeffs(state) ) |> normalize!
+    CQST = QuBase.similar_type(state)
+    return CQST(next_state,bases(state))
+end
 
 end # module
